487c1e490cd8ee3237d823ad1fe0ffb4f3aed079
caperaven <caperaven@gmail.com>
2020-09-04 11:09:20 +0200

    added base for widgets component example

---
+++ app/widgets-component/widgets-component.js
@@ -0,0 +1,7 @@
+import {ViewBase} from "./../../node_modules/crs-binding/crs-view-base.js";
+
+export default class WidgetsComponent extends ViewBase {
+    async connectedCallback() {
+        await super.connectedCallback();
+    }
+}
