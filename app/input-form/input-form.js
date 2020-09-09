3c9c570b969cc84707d0253b7ebb5da1cf39f912
caperaven <caperaven@gmail.com>
2020-09-09 17:17:38 +0200

    add input form example

--- app/input-form/input-form.js
+++ app/input-form/input-form.js
@@ -4,4 +4,9 @@ export default class InputForm extends ExamplesViewBase {
     get sourceUrl() {
         return import.meta.url.replace(".js", ".source.js");
     }
+
+    load() {
+        this.setProperty("person", {firstName: "John", lastName: "Doe", age: 20});
+        super.load();
+    }
 }
