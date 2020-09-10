7899b27f0f6346b3568ec0a67651deb9f6612579
caperaven <caperaven@gmail.com>
2020-09-10 18:44:02 +0200

    update new version

--- build/publish.js
+++ build/publish.js
@@ -2,14 +2,21 @@ const fs = require("fs");
 const mkdirp = require("mkdirp");
 const glob = require("glob");
 const path = require("path");
+const CleanCSS = require('clean-css');
+const terser = require('terser');
 
 class Publish {
     static async distribute() {
         const instance = new Publish();
-        instance.copyFiles("./dist/*.*");
-        instance.copyFiles("./readme.md");
-        //instance.copyFiles("./examples/*.html", "examples");
-        instance.bumpVersion();
+        await instance.copyNodeModules("crs-binding");
+        await instance.copyNodeModules("crs-router");
+        await instance.cleanCSS("./styles/**/*.css");
+        await instance.copyFiles("./dist/*.*");
+        await instance.copyFiles("./readme.md");
+        await instance.copyFiles("./index.html");
+        await instance.copySource("app");
+        await instance.copySource("components");
+        await instance.bumpVersion();
     }
     
     constructor() {
@@ -45,7 +52,7 @@ class Publish {
         });
     }
 
-    bumpVersion() {
+   async bumpVersion() {
         const sourcePackage = `${path.resolve(".")}/package.json`;
         const targetPackage = `${path.resolve(".")}/publish/package.json`;
 
@@ -62,6 +69,63 @@ class Publish {
 
         fs.writeFileSync(sourcePackage, JSON.stringify(pkg, null, 4), "utf8");
         fs.copyFileSync(sourcePackage, targetPackage);
+    }
+
+    async cleanCSS(query, folder) {
+        const cssClean = new CleanCSS({
+            inline: ['all']
+        });
+
+        const files = await this.getFiles(query);
+        for (let file of files) {
+            const basePath = path.dirname(file).replace("./", "");
+            const target = `./publish/${basePath}/`;
+            const fileName = path.basename(file);
+            this.initFolder(target);
+
+            const css = cssClean.minify([file]);
+            fs.writeFileSync(`${target}${fileName}`, css.styles,"utf8");
+        }
+    }
+
+    async copySource(sourceFolder) {
+        const files = await this.getFiles(`./${sourceFolder}/**/*.*`);
+        for (let file of files) {
+            console.log(file);
+
+            const basePath = path.dirname(file).replace("./", "");
+            const target = `./publish/${basePath}/`;
+            const fileName = path.basename(file);
+            const ext = path.extname(file);
+            this.initFolder(target);
+
+            if (ext == ".js") {
+                const code = fs.readFileSync(file, "utf8").toString();
+
+                const settings = {
+                    "mangle": {
+                        "properties": true
+                    }
+                }
+
+                const result = await terser.minify(code, settings);
+                fs.writeFileSync(`${target}${fileName}`, result.code,"utf8");
+            }
+            else {
+                fs.copyFileSync(file, `${target}${fileName}`);
+            }
+        }
+    }
+
+    async copyNodeModules(module) {
+        const files = await this.getFiles(`./node_modules/${module}**/*.*`);
+        for (let file of files) {
+            const basePath = path.dirname(file).replace("./", "");
+            const target = `./publish/${basePath}/`;
+            const fileName = path.basename(file);
+            this.initFolder(target);
+            fs.copyFileSync(file, `${target}${fileName}`);
+        }
     }
 }
 
