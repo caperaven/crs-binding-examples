ac2956e331366c44364694d1592be2f0c5f15a87
caperaven <caperaven@gmail.com>
2020-09-10 23:23:35 +0200

    basic chart example added

--- app/basic-chart/basic-chart.source.js
+++ app/basic-chart/basic-chart.source.js
@@ -6,5 +6,9 @@ export const source = [
     {
         "title": "Basic Chart JS",
         "url": "https://github.com/caperaven/crs-binding-examples/blob/master/app/basic-chart/basic-chart.js",
+    },
+    {
+        "title": "Basic Chart CSS",
+        "url": "https://github.com/caperaven/crs-binding-examples/blob/master/styles/views/basic-chart.css"
     }
 ]
