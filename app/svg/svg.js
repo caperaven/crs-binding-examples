fd0b244e26d39aed55ce7b0652018535a004b4c0
caperaven <caperaven@gmail.com>
2020-09-18 09:57:15 +0200

    update svg example

--- app/svg/svg.js
+++ app/svg/svg.js
@@ -5,4 +5,9 @@ export default class SvgView extends ExamplesViewBase {
     get sourceUrl() {
         return import.meta.url.replace(".js", ".source.js");
     }
+
+    preLoad() {
+        this.setProperty("position", 50);
+        this.setProperty("size", 50);
+    }
 }
