85709cfece54a7fb3b25438446dddfa80c22c5f4
caperaven <caperaven@gmail.com>
2020-10-22 13:22:33 +0200

    adding sync arrays example

--- app/sync-array/sync-array.js
+++ app/sync-array/sync-array.js
@@ -4,4 +4,55 @@ export default class SyncArray extends ExamplesViewBase {
     get sourceUrl() {
         return import.meta.url.replace(".js", ".source.js");
     }
+
+    preLoad() {
+        this.setProperty("available", [
+            {
+                id: 0,
+                title: "item 1",
+                isSelected: true
+            },
+            {
+                id: 1,
+                title: "item 2",
+                isSelected: true
+            },
+            {
+                id: 2,
+                title: "item 3",
+                isSelected: true
+            }
+        ]);
+
+        this.setProperty("syncEnabled", false);
+    }
+
+    load() {
+        crsbinding.data.updateUI(this, "available");
+        crsbinding.data.createArraySync(this, "available", "id", ["title"]).then(syncId => this.syncId = syncId);
+
+        super.load()
+    }
+
+    copySelected() {
+        const selected = this.getProperty("available").filter(item => item.isSelected == true).map(item => {
+            return {
+                id: item.id,
+                title: item.title,
+            }
+        })
+
+        this.setProperty("selected", selected);
+        this.setProperty("hasSelected", selected.length > 0);
+    }
+
+    enableSync() {
+        crsbinding.data.addArraySync(this.syncId, this, "selected");
+        this.setProperty("syncEnabled", true);
+    }
+
+    disableSync() {
+        crsbinding.data.removeArraySync(this.syncId, this, "selected");
+        this.setProperty("syncEnabled", false);
+    }
 }
---
+++ app/sync-array/sync-array.md
@@ -0,0 +1,8 @@
+# Sync Arrays
+
+## Introduction
+
+This scenario shows off a slightly more advanced working on arrays.  
+The first array represents available items.  
+The second array represents selected items.  
+If changes are made on either, apply those changes over both arrays.
